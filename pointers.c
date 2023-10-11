d6adbbba2206910fc8ae8bf3f386e21e98372c87
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-10-11 16:07:39 +0300

    Update comments

--- pointers.c
+++ pointers.c
@@ -2,7 +2,7 @@
 
 int creatingPointers();
 int dereferencingPointers();
-void swapValuesByReference(int *, int *);
+void swapValuesByReference(int*, int*);
 
 int main(void) {
     creatingPointers();
@@ -20,13 +20,16 @@ int creatingPointers() {
     char c = 'c';
     double d = 20.234;
 
-    int * ptrI = &i;
-    char * ptrC = &c;
-    double * ptrD = &d;
+    // create pointers to the three variables
+    int* ptrI = &i;
+    char* ptrC = &c;
+    double* ptrD = &d;
 
-    if (sizeof(ptrI) == 4) {
+    // since a pointer is memory address, it is always a number. Doesn't matter what type points to
+    // depending on os architecture, memory size of a pointer can either be 4 or 8
+    if (sizeof(ptrC) == 4) {
         printf("System is 32bit\n");
-    } else if (sizeof(ptrI) == 8) {
+    } else if (sizeof(ptrC) == 8) {
         printf("System is 64bit\n");
     } else {
         printf("Weird architecture!\n");
@@ -41,7 +44,7 @@ int creatingPointers() {
 
 int dereferencingPointers() {
     int a = 5;
-    int * ptrA = &a;
+    int* ptrA = &a;
 
     // deference i.e read value of throught the pointer
     printf("Value of a through the pointer: %d\n", *ptrA);
@@ -53,7 +56,7 @@ int dereferencingPointers() {
     return 0;
 }
 
-void swapValuesByReference(int * a, int * b) {
+void swapValuesByReference(int* a, int* b) {
     int tmp = *a;
     *a = *b;
     *b = tmp;
